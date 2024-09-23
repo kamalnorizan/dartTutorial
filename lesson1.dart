be5017375895d7d82b6f5c98f58e741e63c22cd7
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2024-09-23 11:29:42 +0800

    method

--- lesson1.dart
+++ lesson1.dart
@@ -1,20 +1,11 @@
 void main() {
   print('Hello World');
 
-  Owner owner1 = Owner(name: 'John', phone: '0123456789', email: 'john@gmail.com');
-  Car car1 = Car(brand: 'Toyota', cc: 1.5, seater: 5, model: 'Vios', owner: owner1);
-
-  printCarDetails(car1);
-}
-
-void printCarDetails(Car car) {
-  print('Car Details');
-  print('Color: ${car.color}');
-  print('Brand: ${car.brand}');
-  print('Model: ${car.model}');
-  print('CC: ${car.cc}');
-  print('Seater: ${car.seater}');
-  print('Owner Name: ${car.owner!.name}');
+  Owner owner1 =
+      Owner(name: 'John', phone: '0123456789', email: 'john@gmail.com');
+  Car car1 =
+      Car(brand: 'Toyota', cc: 1.5, seater: 5, model: 'Vios', owner: owner1);
+  car1.printCarDetails();
 }
 
 class Car {
@@ -32,6 +23,16 @@ class Car {
       required this.cc,
       this.seater,
       this.owner});
+
+  void printCarDetails() {
+    print('Car Details');
+    print('Color: ${this.color}');
+    print('Brand: ${this.brand}');
+    print('Model: ${this.model}');
+    print('CC: ${this.cc}');
+    print('Seater: ${this.seater}');
+    print('Owner Name: ${this.owner!.name}');
+  }
 }
 
 class Owner {
